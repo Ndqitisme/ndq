2f7279a5a5bb34c3559db3e0e1d8334aaeef5ff8
ndqk3itisme <ndqitisme@gmail.com>
2022-06-17 22:28:57 +0700

    Fix bug code commit insert file index

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,18 +1,1 @@
-<<<<<<< HEAD
 export { default } from './GlobalStyles';
-=======
-import PropTypes from 'prop-types';
-
-import './GlobalStyles.scss';
-
-//Nạp Cấu Hình SCSS Cho React App
-function GlobalStyles({ children }) {
-    return children;
-}
-
-GlobalStyles.propTypes = {
-    children: PropTypes.node.isRequired,
-};
-
-export default GlobalStyles;
->>>>>>> 042438b148da7b8ab72209bb1e4d04271b2706cb
